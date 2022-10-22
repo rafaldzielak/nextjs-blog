a5bb282e34975bf9ccf34f6318ccdaf80f6b6c8b
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2022-10-22 17:40:25 +0200

    Style navbar

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,13 +5,19 @@ import styles from "../styles/Layout.module.scss";
 
 const Layout: React.FC<PropsWithChildren> = ({ children }) => {
   return (
-    <main className={styles.container}>
-      <Link href="/">
-        <a>
-          <Image src="/car-logo.png" alt="logo" width={150} height={50} />
-        </a>
-      </Link>
-      <div>{children}</div>
+    <main>
+      <nav className={styles.nav}>
+        <div className={styles.navContent}>
+          <Link href="/">
+            <a>
+              <Image src="/car-logo.png" alt="logo" width={150} height={50} />
+              <h2>RD Car Blog</h2>
+            </a>
+          </Link>
+          <h3>Log in</h3>
+        </div>
+      </nav>
+      <div className={styles.container}>{children}</div>
     </main>
   );
 };
